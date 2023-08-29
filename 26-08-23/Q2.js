469917391e64ae134a1af5c7b86235dd668876de
DINESH THIRU <dineshthiru36@gmail.com>
2023-08-29 08:36:08 -0700

    Test Updated

--- 26-08-23/Q2.js
+++ 26-08-23/Q2.js
@@ -9,3 +9,28 @@ function even(array) {
     return EvenNumbers;
 }
 console.log(even(array))
+
+var array1 = [1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 18]
+function EvenNumbers1(array) {
+    var EvenNumbers1 = [];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] % 2 == 0) {
+            EvenNumbers1.push(array[i])
+        }
+    }
+    return EvenNumbers1;
+}
+console.log(EvenNumbers1(array1))
+
+
+var array2 = [21, 33, 25, 6, 76, 13, 445, 342, 231, 256, 69, 90]
+function EvenNumbers2(array) {
+    var EvenNumbers2 = [];
+    for (i = 0; i < array.length; i++) {
+        if (array[i] % 2 == 0){
+            EvenNumbers2.push(array[i])
+        }
+    }
+    return EvenNumbers2;
+}
+console.log(EvenNumbers2(array2))
---
+++ 26-08-23/Q3.js
@@ -0,0 +1,39 @@
+var num = [23, 48, 11, 87, 30, 53, 8, 34, 65, 15];
+
+function smallNumber(array) {
+    var smallestNum = array[0];
+    for (var i = 1; i < array.length; i++) {
+        // console.log(smallestNum, array[i], smallestNum > array[i])
+        if (smallestNum > array[i]) {
+            smallestNum = array[i];
+        }
+    }
+    return smallestNum;
+}
+console.log(smallNumber(num))
+
+var num1 = [19, 32, 45, 89, 15, 45, 67, 85, 17, 27];
+
+function smallNumber1(array) {
+    var smallestNum1 = array[0];
+    for (var i = 1; i < array.length; i++) {
+        if (smallestNum1 > array[i])[
+            smallestNum1 = array[i]
+        ]
+    }
+    return smallestNum1;
+}
+console.log(smallNumber1(num1))
+
+var num2=[3,5,6,8,9,1,2,4,7]
+
+function smallestNumber2(array){
+    var smallestNum2 = array[0]
+    for (var i=1;i < array.length; i++){
+        if(smallestNum2 > array[i]){
+            smallestNum2 = array[i]
+        }
+    }
+    return smallestNum2;
+}
+console.log(smallestNumber2(num2))
