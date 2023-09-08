5562aa7ac6d1e85a71ac71ee6b8f1caa671158c7
DINESH THIRU <dineshthiru36@gmail.com>
2023-09-08 21:31:43 +0530

    Updated

--- 03-09-23/Q1.js
+++ 03-09-23/Q1.js
@@ -7,14 +7,43 @@
 //     }
 // }
 
-var input = 4;
+
+var input = 6;
 function printStars(num) {
-    for (i = 0; i <= num; i++) {
+    for (var i = 1; i <= num; i++) {
         var pattern = ""
-        for (j = 0; j <= i; j++) {
+        for (j = 1; j <= i; j++) {
             pattern = pattern + "*";
         }
         console.log(pattern)
     }
 }
 printStars(input)
+
+var string = "abcdba";
+function checkP(str) {
+    if (str.length < 2) {
+        return "Str not valid"
+    }
+    for (var i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false
+        }
+    }
+    return true
+}
+console.log(checkP(string))
+
+var string1 = "radar"
+function palindrome(str) {
+    if (str.length < 2) {
+        return "Not valid"
+    }
+    for (var i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false
+        }
+    }
+    return true
+}
+console.log(palindrome(string1))
--- 03-09-23/Q2.js
+++ 03-09-23/Q2.js
@@ -1,16 +1,40 @@
-var input = "racecar"
-j=input.length-1;
-function palindrome(string) {
-    for (i = 0; i <= j; i++) {
+// var input = "racecar"
+// j=input.length-1;
+// function palindrome(string) {
+//     for (i = 0; i <= j; i++) {
         
-            if(string[i] == string[j]){
-                // console.log("true")
-                console.log
-            }else{
-                console.log("false")
-            }
-            j--;
-        }
+//             if(string[i] == string[j]){
+//                 // console.log("true")
+//                 console.log
+//             }else{
+//                 console.log("false")
+//             }
+//             j--;
+//         }
+//     }
+
+// palindrome(input)
+
+// Raju
+
+
+
+var string = "abcba";
+
+function checkP(str) {
+    if(str.length < 2){
+        return "Str not valid"
     }
 
-palindrome(input)
+    for (var i = 0; i < str.length / 2; i++) {
+        if (str[i] !== str[str.length - 1 - i]) {
+            return false
+        }
+    }
+    return true
+
+}
+
+console.log(checkP(string))
+
+
